53a0828f1b455a1dd853ab2217b67b0ae198b9cf
JBShearer <jason.shearer@sap.com>
2026-02-02 22:42:41 -0700

    Generate town layout tiles

--- scripts/World.gd
+++ scripts/World.gd
@@ -7,16 +7,20 @@ extends TileMap
 const GRASS_COLOR := Color("#2F855A")
 const PATH_COLOR := Color("#D9A441")
 const BORDER_COLOR := Color("#1F5A3B")
+const PLAZA_COLOR := Color("#C9A77A")
+const HOUSE_COLOR := Color("#8B5A2B")
 
-const GRASS_SOURCE_ID := 0
-const PATH_SOURCE_ID := 1
-const BORDER_SOURCE_ID := 2
+var grass_source_id: int = -1
+var path_source_id: int = -1
+var border_source_id: int = -1
+var plaza_source_id: int = -1
+var house_source_id: int = -1
 
 func _ready() -> void:
 	collision_layer = 1
 	collision_mask = 1
 	_setup_tileset()
-	_generate_ground()
+	_generate_town()
 
 
 func _setup_tileset() -> void:
@@ -25,16 +29,18 @@ func _setup_tileset() -> void:
 	tileset.set_physics_layer_collision_layer(physics_layer_id, 1)
 	tileset.set_physics_layer_collision_mask(physics_layer_id, 1)
 
-	_add_solid_source(tileset, GRASS_COLOR)
-	_add_solid_source(tileset, PATH_COLOR)
-	_add_solid_source(tileset, BORDER_COLOR)
+	grass_source_id = _add_solid_source(tileset, GRASS_COLOR)
+	path_source_id = _add_solid_source(tileset, PATH_COLOR)
+	border_source_id = _add_solid_source(tileset, BORDER_COLOR)
+	plaza_source_id = _add_solid_source(tileset, PLAZA_COLOR)
+	house_source_id = _add_solid_source(tileset, HOUSE_COLOR)
 
 	self.tile_set = tileset
 	self.tile_set.tile_size = Vector2i(tile_size, tile_size)
 
 
 
-func _add_solid_source(tileset: TileSet, color: Color) -> void:
+func _add_solid_source(tileset: TileSet, color: Color) -> int:
 	var atlas_texture := ImageTexture.create_from_image(_make_solid_tile(color))
 	var source := TileSetAtlasSource.new()
 	source.texture = atlas_texture
@@ -45,7 +51,7 @@ func _add_solid_source(tileset: TileSet, color: Color) -> void:
 	var tile_data := source.get_tile_data(atlas_coords, 0)
 	if tile_data:
 		tile_data.material = null
-		if source_id == BORDER_SOURCE_ID:
+		if source_id == border_source_id:
 			var polygon := PackedVector2Array([
 				Vector2(0, 0),
 				Vector2(tile_size, 0),
@@ -55,6 +61,8 @@ func _add_solid_source(tileset: TileSet, color: Color) -> void:
 			tile_data.set_collision_polygons_count(0, 1)
 			tile_data.set_collision_polygon(0, 0, polygon)
 
+	return source_id
+
 
 func _make_solid_tile(color: Color) -> Image:
 	var image := Image.create(tile_size, tile_size, false, Image.FORMAT_RGBA8)
@@ -62,18 +70,53 @@ func _make_solid_tile(color: Color) -> Image:
 	return image
 
 
-func _generate_ground() -> void:
+func _generate_town() -> void:
 	clear()
 	var offset := Vector2i(-map_width / 2, -map_height / 2)
 	for x in map_width:
 		for y in map_height:
 			var is_border := x == 0 or y == 0 or x == map_width - 1 or y == map_height - 1
 			if is_border:
-				set_cell(0, Vector2i(x, y) + offset, BORDER_SOURCE_ID, Vector2i(0, 0))
+				set_cell(0, Vector2i(x, y) + offset, border_source_id, Vector2i(0, 0))
 				continue
+			set_cell(0, Vector2i(x, y) + offset, grass_source_id, Vector2i(0, 0))
 
-			var is_path := y == map_height / 2 or x == map_width / 2
-			if is_path:
-				set_cell(0, Vector2i(x, y) + offset, PATH_SOURCE_ID, Vector2i(0, 0))
-			else:
-				set_cell(0, Vector2i(x, y) + offset, GRASS_SOURCE_ID, Vector2i(0, 0))
+	_draw_roads(offset)
+	_draw_plaza(offset)
+	_draw_houses(offset)
+
+
+func _draw_roads(offset: Vector2i) -> void:
+	var center_x := map_width / 2
+	var center_y := map_height / 2
+	for x in map_width:
+		var cell := Vector2i(x, center_y) + offset
+		set_cell(0, cell, path_source_id, Vector2i(0, 0))
+	for y in map_height:
+		var cell := Vector2i(center_x, y) + offset
+		set_cell(0, cell, path_source_id, Vector2i(0, 0))
+
+
+func _draw_plaza(offset: Vector2i) -> void:
+	var plaza_size := Vector2i(6, 4)
+	var start := Vector2i(map_width / 2 - plaza_size.x / 2, map_height / 2 - plaza_size.y / 2)
+	for x in plaza_size.x:
+		for y in plaza_size.y:
+			var cell := Vector2i(start.x + x, start.y + y) + offset
+			set_cell(0, cell, plaza_source_id, Vector2i(0, 0))
+
+
+func _draw_houses(offset: Vector2i) -> void:
+	var house_size := Vector2i(4, 3)
+	var padding := Vector2i(4, 4)
+	var positions := [
+		Vector2i(padding.x, padding.y),
+		Vector2i(map_width - house_size.x - padding.x, padding.y),
+		Vector2i(padding.x, map_height - house_size.y - padding.y),
+		Vector2i(map_width - house_size.x - padding.x, map_height - house_size.y - padding.y)
+	]
+	for pos in positions:
+		for x in house_size.x:
+			for y in house_size.y:
+				var cell := Vector2i(pos.x + x, pos.y + y) + offset
+				set_cell(0, cell, house_source_id, Vector2i(0, 0))
